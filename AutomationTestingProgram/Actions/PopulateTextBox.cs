ed17549a3558116fc3e9d5aefd4946aae8722633
Taha Saeed Piracha <Tahasaeed.Piracha@ontario.ca>
2024-12-12 13:33:11 -0500

    PopulateTextBox, SaveFile

---
+++ AutomationTestingProgram/Actions/PopulateTextBox.cs
@@ -0,0 +1,26 @@
+﻿// Exactly the same as PopulateWebElement
+
+using Microsoft.Playwright;
+using System;
+using System.Threading.Tasks;
+
+namespace AutomationTestingProgram.Actions
+{
+    public class PopulateTextBox : IWebAction
+    {
+        public string Name { get; set; } = "PopulateTextBox";
+
+        private readonly PopulateWebElement _populateWebElementAction;
+
+        public PopulateTextBox()
+        {
+            _populateWebElementAction = new PopulateWebElement();
+        }
+
+        public async Task<bool> ExecuteAsync(IPage page, TestStep step, int iteration, Dictionary<string, string> envVars, Dictionary<string, string> saveParams)
+        {
+            // Forward the execution to PopulateWebElement
+            return await _populateWebElementAction.ExecuteAsync(page, step, iteration, envVars, saveParams);
+        }
+    }
+}

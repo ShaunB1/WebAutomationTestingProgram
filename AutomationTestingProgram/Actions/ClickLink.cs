f812e6d4512e0859f600bd5559684cde2b504fe3
Taha Saeed Piracha <Tahasaeed.Piracha@ontario.ca>
2024-12-12 13:29:26 -0500

    ClickLink

---
+++ AutomationTestingProgram/Actions/ClickLink.cs
@@ -0,0 +1,26 @@
+﻿// Exactly the same as Click WebElement
+
+using Microsoft.Playwright;
+using System;
+using System.Threading.Tasks;
+
+namespace AutomationTestingProgram.Actions
+{
+    public class ClickLink : IWebAction
+    {
+        public string Name { get; set; } = "ClickLink";
+
+        private readonly ClickWebElement _clickWebElementAction;
+
+        public ClickLink()
+        {
+            _clickWebElementAction = new ClickWebElement();
+        }
+
+        public async Task<bool> ExecuteAsync(IPage page, TestStep step, int iteration, Dictionary<string, string> envVars, Dictionary<string, string> saveParams)
+        {
+            // Forward the execution to ClickWebElement
+            return await _clickWebElementAction.ExecuteAsync(page, step, iteration, envVars, saveParams);
+        }
+    }
+}
